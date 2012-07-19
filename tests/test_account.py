46235b136fb357f1b4f149cd7b6bfb22db1e2b7d
Paul Boyd <boyd.paul2@gmail.com>
2012-07-18 20:26:41 -0400

    Test encryption
    * tests/test_account.py (testEncryption): Basic encryption tests

--- tests/test_account.py
+++ tests/test_account.py
@@ -32,5 +32,24 @@ class AccountTest(unittest.TestCase):
         a.setPassword('a pass')
         self.assertEqual(a.password(), 'a pass')
 
+    def testEncryption(self):
+        a1 = Account('key', 'crypto')
+        a1.setPassword('pass')
+
+        # Check that the password is encrypted (or at least obscured)
+        encrypted = a1.encryptedPassword()
+        self.assertNotEqual(encrypted, None)
+        self.assertNotEqual(encrypted, 'pass')
+
+        # Make sure the password can be decrypted
+        a2 = Account('key', 'crypto')
+        a2.setEncryptedPassword(encrypted)
+        self.assertEqual(a2.password(), 'pass')
+
+        # Make sure the password can't be decrypted with the wrong key
+        a3 = Account('key', 'wrong key')
+        a3.setEncryptedPassword(encrypted)
+        self.assertNotEqual(a3.password(), 'pass')
+
 if __name__ == "__main__":
     unittest.main()
